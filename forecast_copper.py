07980eb3c779d0f2b8352484ddcf58cf082e34f1
Hst332 <hst332@gmail.com>
2026-01-03 19:51:39 +0100

    Add files via upload

---
+++ forecast_copper.py
@@ -0,0 +1,2 @@
+def forecast_copper_2026():
+    return {"commodity": "Copper", "bull": 45, "neutral": 35, "bear": 20}
---
+++ forecast_gold.py
@@ -0,0 +1,2 @@
+def forecast_gold_2026():
+    return {"commodity": "Gold", "bull": 45, "neutral": 35, "bear": 20}
