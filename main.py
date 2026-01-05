2ae72a0553ec49457786e089fc9a2328df678062
Hst332 <hst332@gmail.com>
2026-01-05 20:44:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from forecast_writer import write_daily_summary
 from forecast_gold import gold_result
 from forecast_silver import silver_result
 from forecast_gas import gas_result
+from forecast_writer import write_daily_summary
 
 def main():
     write_daily_summary([
-        gold_result,
-        silver_result,
-        gas_result
+        gold_result(),
+        silver_result(),
+        gas_result()
     ])
 
 if __name__ == "__main__":
