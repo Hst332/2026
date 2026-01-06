1c14bab24d6d3b97bbb4d6a9b40b9d8f3345ab5a
Hst332 <hst332@gmail.com>
2026-01-06 16:40:07 +0100

    Update forecast_gas.py

--- forecast_gas.py
+++ forecast_gas.py
@@ -15,6 +15,7 @@ def gas_result():
         "date": last.name.strftime("%Y-%m-%d"),
         "close": close,
         "prob_up": prob_up,
+        "model_score": f"{model_score:.2%}",
         "signal": "NO_TRADE",
         "position": "0 %",
         "strategy_lines": [
