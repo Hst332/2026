51e4b3e3304f6a57586eacaad0d0fd5f3e9cd18d
Hst332 <hst332@gmail.com>
2026-01-06 20:14:26 +0100

    Update forecast_gas.py

--- forecast_gas.py
+++ forecast_gas.py
@@ -4,6 +4,7 @@ from forecast_utils import model_score, forecast_trend, trade_signal
 def gas_result():
     df = metals_bundle.load_gas()
     last = df.iloc[-1]
+    close = float(last["Close"])
     score = model_score(df)
 
     return {
