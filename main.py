ca14e0f43db8b9678259cc30f1529fdbe1103a89
Hst332 <hst332@gmail.com>
2026-01-06 14:46:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from forecast_gold import gold_result
 from forecast_silver import silver_result
 from forecast_gas import gas_result
+from forecast_copper import copper_result
 from forecast_writer import write_daily_summary
 
 def main():
@@ -8,6 +9,7 @@ def main():
         gold_result(),
         silver_result(),
         gas_result(),
+        copper_result(),
     ])
 
 if __name__ == "__main__":
