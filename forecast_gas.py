4014d822383d9f457879cc42dad102f080896b1a
Hst332 <hst332@gmail.com>
2026-01-07 16:33:29 +0100

    Update forecast_gas.py

--- forecast_gas.py
+++ forecast_gas.py
@@ -7,7 +7,9 @@ def gas_result():
 
     close = float(df["Close"].iloc[-1])
     date = df.index[-1].strftime("%Y-%m-%d")
-
+    
+    score = model_score(df)
+    
     return {
         "asset": "NATURAL GAS",
         "date": last.name.strftime("%Y-%m-%d"),
