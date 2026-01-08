4a16e62f5b04d9b0355c9b838d5e62986772de9c
Hst332 <hst332@gmail.com>
2026-01-08 07:54:39 +0100

    Update forecast_gas.py

--- forecast_gas.py
+++ forecast_gas.py
@@ -13,12 +13,12 @@ def gold_result():
     score = model_score(df)
 
 
-return {
-"asset": "GOLD",
-"date": datetime.utcnow().strftime("%Y-%m-%d"),
-"close": f"{close:.2f} USD/oz",
-"model_score": f"{score:.2%}",
-"signal": trade_signal(score),
-"forecast_1_5d": forecast_trend(df, 5),
-"forecast_2_3w": forecast_trend(df, 15),
-}
+    return {
+        "asset": "GOLD",
+        "date": datetime.utcnow().strftime("%Y-%m-%d"),
+        "close": f"{close:.2f} USD/oz",
+        "model_score": f"{score:.2%}",
+        "signal": trade_signal(score),
+        "forecast_1_5d": forecast_trend(df, 5),
+        "forecast_2_3w": forecast_trend(df, 15),
+        }
